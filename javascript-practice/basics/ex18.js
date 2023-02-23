c84350ffc68ece3836dd12878bf53dff936eb5dd
awlals <awlals616@gmail.com>
2023-02-23 14:54:38 +0900

    js + browser programming

---
+++ javascript-practice/basics/ex18.js
@@ -0,0 +1,13 @@
+/**
+ * 전역 객체(Global Object)
+ * 
+ * node: global                안에 베리어블스? 
+ * browser: window
+ * 
+ */
+
+global.n = '둘리';
+console.log(global.n, n);
+
+var email = 'dooly@email.com';
+console.log(global.email, email)
